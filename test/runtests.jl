380223a92163512c8a331af82c0484ebd11b774c
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2016-08-22 03:41:34 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,18 @@
 using TextSearch
+using Compat
 using Base.Test
 
 cd(Pkg.dir("TextSearch"))
 index_directory("examples", ".jl")
 
-@test S"∪" & S"∩" == Set(Union{ASCIIString,UTF8String}["..\\examples\\bazbar.jl"])
-@test S"∪" | S"end" == Set(Union{ASCIIString,UTF8String}["..\\examples\\qux.jl","..\\examples\\bar.jl","..\\examples\\foobarbaz.jl","..\\examples\\foo.jl","..\\examples\\foobar.jl","..\\examples\\bazbar.jl"])
-@test S"function" - S"=" == Set(Union{ASCIIString,UTF8String}["..\\examples\\foo.jl","..\\examples\\foobar.jl"])
-@test S"else" & !S"#" == Set(Union{ASCIIString,UTF8String}["..\\examples\\bar.jl"])
+@static if is_windows()
+    @test S"∪" & S"∩" == Set(Union{ASCIIString,UTF8String}["examples\\bazbar.jl"])
+    @test S"∪" | S"end" == Set(Union{ASCIIString,UTF8String}["examples\\qux.jl",
+                                                             "examples\\bar.jl",
+                                                             "examples\\foobarbaz.jl",
+                                                             "examples\\foo.jl",
+                                                             "examples\\foobar.jl",
+                                                             "examples\\bazbar.jl"])
+    @test S"function" - S"=" == Set(Union{ASCIIString,UTF8String}["examples\\foo.jl","examples\\foobar.jl"])
+    @test S"else" & !S"#" == Set(Union{ASCIIString,UTF8String}["examples\\bar.jl"])
+end
